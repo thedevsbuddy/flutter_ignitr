c3880f044657b4d2fd1702986e9642528083b5e9
Devsbuddy <shoabkhan33@gmail.com>
2025-01-19 22:49:11 +0530

    WIP: Added Database with ORM for Local File Storage

--- lib/app/modules/auth/services/local_auth_service.dart
+++ lib/app/modules/auth/services/local_auth_service.dart
@@ -9,12 +9,11 @@ class LocalAuthService extends BaseService implements AuthService {
   String? database = 'users';
 
   @override
-  Future<ApiResponse> login(
-      {required String client, required Map<String, dynamic> body}) async {
-    Map<String, dynamic>? _data = await db.findOne({
+  Future<ApiResponse> login({required String client, required Map<String, dynamic> body}) async {
+    Map<String, dynamic>? _data = await db.where({
       'email': body['identifier'],
       'password': body['password'],
-    });
+    }).first();
 
     if (_data != null) {
       UserModel user = UserModel.fromJson(_data);
@@ -29,19 +28,15 @@ class LocalAuthService extends BaseService implements AuthService {
           );
     }
 
-    return await 300
-        .milliseconds
-        .delay(() => ApiResponse.error(message: "Invalid Credentials"));
+    return await 300.milliseconds.delay(() => ApiResponse.error(message: "Invalid Credentials"));
   }
 
   @override
-  Future<ApiResponse> register(
-      {required String client, required Map<String, dynamic> body}) async {
-    List<Map<String, dynamic>>? users = await db.findMany();
+  Future<ApiResponse> register({required String client, required Map<String, dynamic> body}) async {
+    List<Map<String, dynamic>>? users = await db.get();
 
     int newId = 1;
-    if (users != null && users.isNotEmpty)
-      newId = int.parse(users.last['id'].toString()) + 1;
+    if (users.isNotEmpty) newId = int.parse(users.last['id'].toString()) + 1;
 
     List<Map<String, dynamic>>? _data = await db.store(
       UserModel.fromJson(body)
@@ -57,10 +52,7 @@ class LocalAuthService extends BaseService implements AuthService {
   }
 
   @override
-  Future<ApiResponse> verifyOtp(
-      {required String client, required Map<String, dynamic> body}) async {
-    return await 300
-        .milliseconds
-        .delay(() => ApiResponse.success(message: "OTP Verified"));
+  Future<ApiResponse> verifyOtp({required String client, required Map<String, dynamic> body}) async {
+    return await 300.milliseconds.delay(() => ApiResponse.success(message: "OTP Verified"));
   }
 }
--- lib/app/modules/dashboard/controllers/dashboard_controller.dart
+++ lib/app/modules/dashboard/controllers/dashboard_controller.dart
@@ -1,13 +1,12 @@
 import 'package:get/get.dart';
 
-import '../../../shared/controllers/app_controller.dart';
-import '../../../shared/views/errors/errors.dart';
+import '../../../../helpers/helpers.dart';
+import '../../../shared/shared.dart';
 import '../../modules.dart';
 
 class DashboardController extends AppController {
   static DashboardController get instance {
-    if (!Get.isRegistered<DashboardController>())
-      Get.put(DashboardController());
+    if (!Get.isRegistered<DashboardController>()) Get.put(DashboardController());
     return Get.find<DashboardController>();
   }
 
@@ -22,8 +21,18 @@ class DashboardController extends AppController {
   Future<void> getData() async {
     try {
       String _client = 'dashboard-get-data';
+      setBusy(true);
       _dashboardService.init(_client);
-      await _dashboardService.doSomething(client: _client);
+      ApiResponse response = await _dashboardService.getData(client: _client);
+      if (response.hasError()) {
+        Toastr.error(message: "${response.message}");
+        _dashboardService.close(_client);
+        setBusy(false);
+        return;
+      }
+      print(response.data);
+      setBusy(false);
+      update();
       _dashboardService.close(_client);
     } on Exception catch (e) {
       Get.to(() => ErrorPage(message: "$e"));
